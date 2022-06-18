f6bfc18ed8fd83f374bb07be32ff1d09c694d70a
Matthew Anthony <misterbaboo@gmail.com>
2022-06-17 22:44:04 -0400

    Add Terms & Conditions modal

---
+++ src/js/imports.js
@@ -0,0 +1,6 @@
+$(function () {
+	$("#header-import").load("/header.html");
+});
+$(function () {
+	$("#footer-import").load("/footer.html");
+});
---
+++ src/js/sign-up.js
@@ -0,0 +1,24 @@
+function openModal() {
+	var element = document.getElementById("terms-modal");
+  element.classList.add("is-active");
+}
+
+function closeModal() {
+	var element = document.getElementById("terms-modal");
+  element.classList.remove("is-active");
+}
+
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+    if (e.key === "Escape") { // Escape key
+      closeAllModals();
+    }
+  });
