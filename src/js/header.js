3820925b9b19aca9e83695e9f0f32c86429705a5
Matthew Anthony <misterbaboo@gmail.com>
2022-07-20 11:16:02 -0400

    Attempt to fix dropdown on mobile

--- src/js/header.js
+++ src/js/header.js
@@ -8,3 +8,30 @@ function toggleNav() {
 			nav.classList.remove("is-active");
 	}
 }
+
+/** The jQuery will trigger only at mobile viewport */
+if (window.matchMedia("(max-width: 767px)").matches) {    
+	const $navDropdowns = document.querySelectorAll(".navbar-item.has-dropdown");
+	if ($navDropdowns.length > 0) {
+		// HIDE THE DROP-DOWN ON THE INITIAL LOAD
+		$navDropdowns.forEach((el) => {
+			const target = el.dataset.target;
+			const $target = document.getElementById(target);
+			$target.style.display = "none";
+
+			//Register the click event on the dropdown list
+			el.addEventListener("click", () => {
+				// Get the target from the "data-target" attribute
+				const target = el.dataset.target;
+				const $target = document.getElementById(target);
+				
+				if ($target.style.display === "block") {
+					$target.style.display = "none";
+				} else {
+					$target.style.display = "block";
+				}
+			});
+		});
+	}
+}
+	
